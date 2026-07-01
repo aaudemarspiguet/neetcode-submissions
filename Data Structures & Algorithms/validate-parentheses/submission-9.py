c6389441a173391c9fdec3f064479c706622ffac
aaudemarspiguet <106708408+aaudemarspiguet@users.noreply.github.com>
2026-07-01 13:24:13 -0400

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for char in s:
+            if char == '{' or char == '(' or char == '[':
+                stack.append(char)
+                continue
+            elif len(stack) != 0 and char == '}' and stack[-1] == '{':
+                stack.pop()
+                continue
+            elif len(stack) != 0 and char == ']' and stack[-1] == '[':
+                stack.pop()
+                continue
+            elif len(stack) != 0 and char == ')' and stack[-1] == '(':
+                stack.pop()
+                continue
+            return False
+
+        return True if not stack else False
