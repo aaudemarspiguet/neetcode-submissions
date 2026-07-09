1dd941c7e643c23fcaa2a614c7f391c3cfc48963
aaudemarspiguet <106708408+aaudemarspiguet@users.noreply.github.com>
2026-07-09 10:24:09 -0400

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,39 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
+        # if the problem were simple and tomorrow is always hotter than today, we can return an array of 1s
+        # instead, we might have a mix: tomorrow's weather might be colder than today's
+
+        # approach: 
+        # immediately compute result[i] for when tomorrow is hotter than today (when day i+1 is hotter than day i)
+        # we'll skip over a day if the following day is not immediately and come back to it
+        # in other words, we'll need a data structure to track days for which we haven't found a hotter day yet
+        # thus, every value in this data structure, if being appended to must be in decreasing order. 
+
+        # voila: monotonic stack
+
+        # NOTE: last index is always 0
+
+        # this stack keeps track of days j for which we've yet to see a hotter day, 
+        #                                               result[j] is 0 at the moment
+        stack = [] 
+        result = [0] * len(temperatures)
+
+        # algorithm:
+
+        for i in range(0, len(temperatures), 1):
+            print(f"Stack: {stack}")
+            print(f"Result: {result}")
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                popped_index = stack.pop()
+                result[popped_index] = i - popped_index
+
+            stack.append(i)
+
+        return result
+        
+
+
+
+
+
